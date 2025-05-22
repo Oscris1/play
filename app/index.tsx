79fc97ca8e3b7ba8d9bd3335fd856c7c8f91f884
Damian <damianpietka@gmail.com>
2025-05-23 01:42:21 +0200

    feat: prepare main screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,20 @@
 import { Stack } from 'expo-router'
-import { View } from 'react-native';
-
+import { Button, H1, YStack } from 'tamagui';
+import  SafeArea  from '~/components/SafeArea';
 
 
 
 export default function Home() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Home' }} />
-     <View />
+      <Stack.Screen options={{ headerShown: false }} />
+      <SafeArea>
+      <YStack flex={1}  alignItems="center" backgroundColor="white" gap={16} padding={16}>
+          <H1>Play</H1>
+          <Button width="100%" theme="blue">Formularz</Button>
+          <Button width="100%" theme="blue">Api</Button>
+      </YStack>
+      </SafeArea>
     </>
   );
 }
