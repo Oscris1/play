270b404a39619a3208fec387d17b1bd4b907369a
Damian <damianpietka@gmail.com>
2025-05-23 01:49:51 +0200

    feat: add icons to buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Stack } from 'expo-router'
 import { Button, H1, YStack } from 'tamagui';
 import  SafeArea  from '~/components/SafeArea';
-
+import { FormInput, Antenna } from '@tamagui/lucide-icons';
 
 
 export default function Home() {
@@ -11,8 +11,8 @@ export default function Home() {
       <SafeArea>
       <YStack flex={1}  alignItems="center" backgroundColor="white" gap={16} padding={16}>
           <H1>Play</H1>
-          <Button width="100%" theme="blue">Formularz</Button>
-          <Button width="100%" theme="blue">Api</Button>
+          <Button width="100%" theme="blue" iconAfter={<FormInput />}>Formularz</Button>
+          <Button width="100%" theme="blue" iconAfter={<Antenna />}>Api</Button>
       </YStack>
       </SafeArea>
     </>
