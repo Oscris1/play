268c9d6172c854e4fa0371c83d1ed6446419772f
Damian <damianpietka@gmail.com>
2025-05-23 01:15:41 +0200

    chore: initial cleanup

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,14 @@
-import { Stack, Link } from 'expo-router';
+import { Stack } from 'expo-router'
+import { View } from 'react-native';
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+
+
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+     <View />
     </>
   );
 }
