74b0584ce682a909233790574d72482839dea72e
Damian <damianpietka@gmail.com>
2025-05-23 02:58:25 +0200

    feat: prepare validation schema

--- app/formScreen.tsx
+++ app/formScreen.tsx
@@ -1,7 +1,19 @@
 import { Stack } from "expo-router"
 import { YStack } from "tamagui"
+import * as Yup from 'yup'
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().min(5, 'Imię musi mieć co najmniej 5 znaków').required('Imię jest wymagane'),
+  email: Yup.string().email('Nieprawidłowy adres email').required('Email jest wymagany'),
+})
 
 const FormScreen = () => {
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(validationSchema),
+  })
+
   return (
     <>
       <Stack.Screen options={{ title: 'Formularz' }} />
